47ba8ba6c87d6b45aa16e2898b4e8e77f31923d6
toran billups <toranb@gmail.com>
2018-02-22 05:05:37 -0600

    ember-cli-typescript v1.1.5 & new seamless typedefs

--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -1,13 +1,14 @@
 import { combineReducers } from 'redux';
 import Immutable from 'seamless-immutable';
+import { NumberState, AddAction, ImmutableState } from '../types/index';
 
 const initialState = Immutable({
   value: 0
 });
 
-const number = ((state, action) => {
+const number = ((state: ImmutableState<NumberState>, action: AddAction): ImmutableState<NumberState> => {
   if(action.type === 'ADD') {
-    return state.merge({value: state.value + 1});
+    return state.merge({value: state.value + 2});
   }
   return state || initialState;
 });
